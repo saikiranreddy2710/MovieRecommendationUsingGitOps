ce679b3423b42c4876f7271f60b05cb4213807cc
Saikiran Reddy <96383342+saikiranreddy2710@users.noreply.github.com>
2024-04-08 09:48:03 +0530

    Update moviesInfo.py

--- moviesInfo.py
+++ moviesInfo.py
@@ -2,7 +2,7 @@ from tmdbv3api import TMDb,Movie
 import json
 import requests
 tmdb = TMDb()
-tmdb.api_key = 'api_key'
+tmdb.api_key = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4N2YzN2JkODZmM2Y5M2MyMjQ2MWIzMmRiZDYyOGYzOCIsInN1YiI6IjY1OGMyY2RkMjcxNjcxNzFkNmE0ZmM4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.n4Tw7wuQWbtR01FakLsIetrHU68FrbaDAnJ6-awUM9k'
 class MovieInfo:
     def __init__(self):
         self.tmdbObj= Movie()
